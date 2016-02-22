29023ecb526b10d9c537787f7949d1e9e46fee0b
Pixel <louis.vincent.bompart@gmail.com>
2016-02-22 23:16:07 +0100

    Solve 05-00
    And done, good night !

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,25 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne === stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return !(one === two);
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return (middle>lower) && (middle<upper);
 };
 
 module.exports.outsideRanges = function(number) {
-
+  if(number>1 && number<6)
+    return false;
+  if(number>42 && number<=75)
+    return false;
+  if(number>=10 && number<=20)
+    return false;
+  return true;
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  return price>=1 && (name==="NYTimes" || name==="LATimes");
 };
